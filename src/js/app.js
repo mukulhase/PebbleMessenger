6a0e83d81ea2b1936132640660a8557d77dced99
Mukul Hase <mukulhase@gmail.com>
2017-07-22 00:51:14 +0530

    SEcond

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,39 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
 
 var UI = require('ui');
 var Vector2 = require('vector2');
+var ajax = require('ajax');
+
+
 
 var main = new UI.Card({
   title: 'Messenger for Pebble',
   icon: 'images/menu_icon.png',
-  subtitle: 'Who do you want to chat with?',
-  body: 'Press any button.',
+  subtitle: 'Loading Chats',
+  body: 'Please Wait....',
   subtitleColor: 'indigo', // Named colors
   bodyColor: '#9a0036' // Hex colors
 });
-
 main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
+ajax({ url: 'https://test.mukulhase.com/list', type: 'json' },
+  function(data, status, req) {
+    var items = data.map(function(obj){
+      return {
+        title: obj.name,
+        subtitle: obj.snippet
+      };
+    });
+    var menu = new UI.Menu({
+      sections: [{
+        items: items
       }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+    });
+    menu.on('select', function(e) {
+      console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+      console.log('The item is titled "' + e.item.title + '"');
+    });
+    menu.show();
+  }
+);
 
 main.on('click', 'select', function(e) {
   var wind = new UI.Window({
